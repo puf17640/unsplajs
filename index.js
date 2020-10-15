cebcdfce96841d2f18b812c6f9ca275e6dda82a4
Julian Pufler <puf17640@spengergasse.at>
2020-10-15 20:48:25 +0200

    added topic methods and made http private

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const http = require('./utils/http'),
-	{ User, Photo } = require('./models');
+	{ User, Photo, Collection, Topic } = require('./models');
 
 class Unsplash {
+	#http;
 	constructor(accessKey) {
-		this.http = http;
+		this.#http = http;
 		http.setAuth(accessKey);
 	}
 
 	async getUser(username) {
-		const user = await http.get(`/users/${username}`);
+		const user = await this.#http.get(`/users/${username}`);
 		return new User(user);
 	}
 
 	async getPhoto(id) {
-		const photo = await http.get(`/photos/${id}`);
+		const photo = await this.#http.get(`/photos/${id}`);
 		return new Photo(photo);
+	}
+
+	async getCollection(id) {
+		const collection = await this.#http.get(`/collections/${id}`);
+		return new Collection(collection);
 	}
 
 	async getRandomPhoto() {
-		const photo = await http.get('/photos/random');
+		const photo = await this.#http.get('/photos/random');
 		return new Photo(photo);
 	}
 
+	async getTopic(slugOrId) {
+		const topic = await this.#http.get(`/topics/${slugOrId}`);
+		return new Topic(topic);
+	}
+
+	async getTopics() {
+		const topics = await this.#http.get('/topics');
+		return topics.map(t => new Topic(t));
+	}
 }
 
 module.exports = Unsplash;
