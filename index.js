228c88aa028314f412b24168297a2d9231ebbd87
Julian Pufler <puf17640@spengergasse.at>
2020-10-20 09:29:00 +0200

    added search methods and made http property private

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('./utils/http'),
-	{ User, Photo, Collection, Topic, UnsplashStatistics } = require('./models');
+	{ User, Photo, Collection, Topic, UnsplashStatistics, UserSearchResult, PhotoSearchResult, CollectionSearchResult } = require('./models');
 
 class Unsplash {
 	#http;
 	constructor(accessKey) {
 		this.#http = http;
-		http.setAuth(accessKey);
+		this.#http.setAuth(accessKey);
 	}
 
 	async getUser(username) {
@@ -13,9 +13,29 @@ class Unsplash {
 		return new User(user);
 	}
 
+	async searchUsers(opts = {}) {
+		const search = await this.#http.get(`/search/users?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return new UserSearchResult(search);
+	}
+
+	async getPhotos(opts = { page: 1, perPage: 10, orderBy: 'latest' }) {
+		const photos = await this.#http.get(`/photos?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return photos.map(p => new Photo(p));
+	}
+
 	async getPhoto(id) {
 		const photo = await this.#http.get(`/photos/${id}`);
 		return new Photo(photo);
+	}
+
+	async searchPhotos(opts = {}) {
+		const search = await this.#http.get(`/search/photos?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return new PhotoSearchResult(search);
+	}
+
+	async getCollections(opts = { page: 1, perPage: 10 }) {
+		const collections = await this.#http.get(`/collections?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return collections.map(p => new Collection(p));
 	}
 
 	async getCollection(id) {
@@ -23,9 +43,19 @@ class Unsplash {
 		return new Collection(collection);
 	}
 
-	async getRandomPhoto() {
-		const photo = await this.#http.get('/photos/random');
-		return new Photo(photo);
+	async searchCollections(opts = {}) {
+		const search = await this.#http.get(`/search/collections?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return new CollectionSearchResult(search);
+	}
+
+	async getRandomPhoto(opts = {}) {
+		const res = await this.#http.get(`/photos/random?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return Array.isArray(res) ? res.map(p => new Photo(p)) : new Photo(res);
+	}
+
+	async getTopics(opts = { page: 1, perPage: 10, orderBy: 'position' }) {
+		const topics = await this.#http.get(`/topics?${Object.entries(opts).map(([k, v]) => `${k}=${v}`).join('&')}`);
+		return topics.map(t => new Topic(t));
 	}
 
 	async getTopic(slugOrId) {
@@ -33,14 +63,13 @@ class Unsplash {
 		return new Topic(topic);
 	}
 
-	async getTopics() {
-		const topics = await this.#http.get('/topics');
-		return topics.map(t => new Topic(t));
-	}
-
 	async getStatistics(monthly = false) {
 		const stats = await this.#http.get(`/stats/${monthly ? 'month' : 'total'}`);
 		return new UnsplashStatistics(stats);
+	}
+
+	get requestsRemaining() {
+		return this.#http.ratelimit.remaining;
 	}
 }
 
